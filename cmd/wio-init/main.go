315f9bf1c4ae5d4d6e8f999c34ab751e74c36348
Daisuke (yet another) Maki <maki.daisuke+gh@gmail.com>
2019-11-07 18:17:20 -0600

    Friendlier error messages in wio-init

--- cmd/wio-init/main.go
+++ cmd/wio-init/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"os"
 	"path/filepath"
 
@@ -19,16 +20,27 @@ func main() {
 		name = filepath.Base(cwd)
 	}
 
-	var err error
-	err = wio.WriteMainGo(name, "0.0.1")
+	err := wio.WriteMainGo(name, "0.0.1")
 	if err != nil {
-		panic(err)
+		if os.IsExist(err) {
+			fmt.Fprintln(os.Stderr, `Initialization failed: main.go already exists. Run wio-init in an empty directory.`)
+			os.Exit(1)
+		} else {
+			panic(err)
+		}
 	}
-	err = wio.WritePackGo([]byte{})
-	if err != nil {
-		panic(err)
-	}
+
 	err = wio.WritePackingList(name)
+	if err != nil {
+		if os.IsExist(err) {
+			fmt.Fprintln(os.Stderr, `Initialization failed: packing-list already exists. Run wio-init in an empty directory.`)
+			os.Exit(1)
+		} else {
+			panic(err)
+		}
+	}
+
+	err = wio.WritePackGo([]byte{})
 	if err != nil {
 		panic(err)
 	}
